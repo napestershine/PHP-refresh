fc5ddc5601cb31a3533c727fea4b4c21aeed5b2b
shine <napestershine@gmail.com>
2018-07-27 22:30:51 +0530

    added DI

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,17 +1,9 @@
 <?php
 
 
-$app = [];
+App::bind('config', require 'config.php');
 
 
-$app['config'] = require 'config.php';
-
-require 'core/Router.php';
-require 'core/Request.php';
-require 'core/database/Connection.php';
-require 'core/database/QueryBuilder.php';
-
-
-$app['database'] =  new QueryBuilder(
-    Connection::make($app['config']['database'])
-);
+App::bind('database', new QueryBuilder(
+    Connection::make(App::get('config')['database'])
+));
